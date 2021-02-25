b1235c13591ce401b0a51e26110943727b7777d6
Warren, Ben <bwarren@eab.com>
2021-02-24 22:41:02 -0500

    Add logging

--- pipelines_webinar/lambda/handler.py
+++ pipelines_webinar/lambda/handler.py
@@ -1,8 +1,12 @@
-import logging
+import logging, os
 
 logger = logging.getLogger("handler")
+logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    logger.info(event, context)
+    logger.info(event)
+    logger.info(context)
+    logger.info(os.environ)
+
     return {"body": "Hello from limmy lams", "statusCode": "200"}
