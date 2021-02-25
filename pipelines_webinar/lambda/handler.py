f72d7a866c300c97a891994036717cb959e0932d
Warren, Ben <bwarren@eab.com>
2021-02-24 21:50:09 -0500

    Changes

--- pipelines_webinar/lambda/handler.py
+++ pipelines_webinar/lambda/handler.py
@@ -1,2 +1,8 @@
+import logging
+
+logging.getLogger("handler")
+
+
 def handler(event, context):
+    logger.info("Invoked ze lambda")
     return {"body": "Hello king from limmy lams", "statusCode": "200"}
