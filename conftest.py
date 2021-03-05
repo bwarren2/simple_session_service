71c36cf9d1840893dc977bc13014e27753035270
Warren, Ben <bwarren@eab.com>
2021-03-05 00:32:27 -0500

    Move conftest

---
+++ conftest.py
@@ -0,0 +1,6 @@
+import os, sys
+
+here = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "pipelines_webinar", "lambdas"
+)
+sys.path.insert(0, here)
--- pipelines_webinar/conftest.py
+++
@@ -1,4 +0,0 @@
-import os, sys
-
-here = os.path.join(os.path.dirname(os.path.abspath(__file__)), "lambdas")
-sys.path.insert(0, here)
