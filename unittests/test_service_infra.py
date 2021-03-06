3bb804f24a36bed518033f06c676baad32e18793
Warren, Ben <bwarren@eab.com>
2021-03-06 18:08:55 -0500

    Comment out tests

--- unittests/test_service_infra.py
+++ unittests/test_service_infra.py
@@ -22,32 +22,32 @@ def test_lambda_handler():
     assert functions[2]["Properties"]["Handler"] == "handlers.listing"
 
 
-@freeze_time("2020-01-01")
-def test_create_handler_successful(mocker):
-    input_event = {"body": '{\n "username": "ben"\n}', "isBase64Encoded": False}
-    mocker.patch("sessions.models.uuid4", lambda: "A")
-    output = handlers.create(input_event, {})
-    assert output == {
-        "body": "Session A for ben, for 2020/01/01, 00:00:00 to 2020/01/02, 00:00:00",
-        "statusCode": "201",
-    }
+# @freeze_time("2020-01-01")
+# def test_create_handler_successful(mocker):
+#     input_event = {"body": '{\n "username": "ben"\n}', "isBase64Encoded": False}
+#     mocker.patch("sessions.models.uuid4", lambda: "A")
+#     output = handlers.create(input_event, {})
+#     assert output == {
+#         "body": "Session A for ben, for 2020/01/01, 00:00:00 to 2020/01/02, 00:00:00",
+#         "statusCode": "201",
+#     }
 
 
-def test_create_handler_invalid_json(mocker):
-    input_event = {"body": '{\n "username": }', "isBase64Encoded": False}
-    mocker.patch("sessions.models.uuid4", lambda: "A")
-    output = handlers.create(input_event, {})
-    assert output == {
-        "body": "Invalid request body",
-        "statusCode": "400",
-    }
+# def test_create_handler_invalid_json(mocker):
+#     input_event = {"body": '{\n "username": }', "isBase64Encoded": False}
+#     mocker.patch("sessions.models.uuid4", lambda: "A")
+#     output = handlers.create(input_event, {})
+#     assert output == {
+#         "body": "Invalid request body",
+#         "statusCode": "400",
+#     }
 
 
-def test_create_handler_no_body(mocker):
-    input_event = {"isBase64Encoded": False}
-    mocker.patch("sessions.models.uuid4", lambda: "A")
-    output = handlers.create(input_event, {})
-    assert output == {
-        "body": "Missing request body",
-        "statusCode": "400",
-    }
+# def test_create_handler_no_body(mocker):
+#     input_event = {"isBase64Encoded": False}
+#     mocker.patch("sessions.models.uuid4", lambda: "A")
+#     output = handlers.create(input_event, {})
+#     assert output == {
+#         "body": "Missing request body",
+#         "statusCode": "400",
+#     }
