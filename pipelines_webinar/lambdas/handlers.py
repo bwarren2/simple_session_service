80621b61570dfcf52f4aa4feaa5ea5e110d9a579
Warren, Ben <bwarren@eab.com>
2021-03-08 23:19:49 -0500

    Don't print retrieve

--- pipelines_webinar/lambdas/handlers.py
+++ pipelines_webinar/lambdas/handlers.py
@@ -56,7 +56,7 @@ def retrieve(event, context):
         TableName=os.getenv("SESSION_TABLE_NAME"),
         Key={"SessionToken": {"S": "f5d5189c-6a07-4666-85ae-797029cc3862"}},
     )
-    logger.info(response)
+    # logger.info(response)
     logger.info(event)
     logger.info(context)
     logger.info(dir(response))
